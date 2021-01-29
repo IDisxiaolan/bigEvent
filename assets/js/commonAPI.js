87ee89cb27f51d2547cc965570658352317464f1
xiaolan <827920850@qq.com>
2021-01-29 15:26:29 +0800

    新增index.html,css,js

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -3,6 +3,10 @@ axios.defaults.baseURL = 'http://ajax.frontend.itheima.net'
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
 
+    var token = localStorage.getItem('token') || ''
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
     return config;
 }, function(error) {
     // 对请求错误做些什么
@@ -12,6 +16,12 @@ axios.interceptors.request.use(function(config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
     // 对响应数据做点什么
+    const { message, status } = response.data
+    if (message == '身份认证失败！' && status == 1) {
+        //跳转在清除token
+        location.href = 'login.html'
+        localStorage.removeItem('token')
+    }
     return response.data;
 }, function(error) {
     // 对响应错误做点什么
---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+$(() => {
+    function getUserInfo() {
+
+        const layer = layui
+        axios.get('/my/userinfo')
+            .then(function(res) {
+                console.log(res);
+                if (res.status !== 0) { return layer.msg('获取用户信息失败!') }
+                const { data } = res
+
+                const name = data.nickname || data.username
+                    //2.昵称渲染
+                $('.nickname').text(`欢迎${name}`)
+                    //3.头像
+                if (data.user_pic) {
+                    $('.avatar').attr('src', data.user_pic).show()
+                    $('.text-avatar').hide()
+                } else {
+                    $('.avatar').hide()
+                    $('.text-avatar').text(name[0].toUpperCase()).show()
+                }
+            })
+
+    }
+    getUserInfo()
+
+    $('#tuichu').click(function() {
+        localStorage.removeItem('token')
+        location.href = 'login.html'
+    })
+})
